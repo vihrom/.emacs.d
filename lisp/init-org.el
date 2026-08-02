a362ad5b80c4843e821f6fcd6b664048fe288aa8
Vitalik Khatuntsev <vitalik.khatuntsev@gmail.com>
2026-08-02 13:52:30 +0300

    minor fix superstar-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -7,21 +7,23 @@
   (setq org-agenda-files '("~/org/tasks.org"))
   (setq org-log-done 'time)
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "IN-PROGRESS(i)" "|" "DONE(d)" "CANCELLED(c)"))))
+	'((sequence "TODO(t)" "IN-PROGRESS(i)" "|" "DONE(d)" "CANCELLED(c)"))))
 
 (use-package evil-org
   :after org
   :hook (org-mode . evil-org-mode)
   :config
   (add-hook 'evil-org-mode-hook
-            (lambda ()
-              (evil-org-set-key-theme '(navigation insert textobjects additional calendar))))
+	    (lambda ()
+	      (evil-org-set-key-theme '(navigation insert textobjects additional calendar))))
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
 
 (use-package org-superstar
   :after org
-  :hook (org-mode . org-superstar-mode))
+  :hook (org-mode . org-superstar-mode)
+  :config
+  (setq org-superstar-leading-bullet " "))
 
 (provide 'init-org)
 ;;; init-org.el ends here
